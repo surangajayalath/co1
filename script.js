0d80a08f588ca2f49e94b71c0e39893e335f6e3f
suranga <56903228+surangajayalath@users.noreply.github.com>
2021-01-23 22:52:05 +0530

    Update script.js

--- script.js
+++ script.js
@@ -4,22 +4,45 @@ function compute()
      t = document.getElementById("years").value;
       r = document.getElementById("rate").value;
 
-      	a = p;
+
+
+      /*if(p = ''){
+      	alert("Please Enter Positive Number")
+      }*/
+
+     if(p == ''){
+     	alert("Please Enter Positive Number");
+     }
+     else if(p <= 0){
+     	alert("Please Enter Positive Number");
+     }
+     else{
+     	a = p;
+     }
+
+
+
+      	//a = p;
       	b = t;
       	c = r;
 	x = a * b * c / 100;
 	
-	m1 = "If You deposit ";
+	m1 = "If you deposit ";
 	m2 = " at an interest rate of ";
 	m3 = " You will receive an amount of ";
 	m4 = " in the year ";
 	xc = 202 + b;
-	br = "<br>"
+	br = "<br>";
+	co =",";
+	dot=".";
+	pr = "%";
+	b= "<span class ='ye'>"
+	bb ="</span>"
 
-	m11 = m1 + a;
-	m22 = m2 + r;
-	m33 = m3 + x;
-	m44 = m4 +xc;
+	m11 = m1 + b + a +bb +co;
+	m22 = m2 + b+ r+pr+bb+dot;
+	m33 = m3 + b+ x +bb +co;
+	m44 = m4 + b+xc+bb;
 
 	k = m11 + br + m22 + br + m33 + br + m44;
 	document.getElementById("prin").innerHTML = k;
